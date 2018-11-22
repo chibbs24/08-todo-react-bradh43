74c934b4a2666c9fd10a4fd567e09c8b2ef1a918
Brad Hodkinson <bradh@wustl.edu>
2018-11-21 20:59:50 -0600

    Updates

---
+++ precache-manifest.a19744a2304cabfafb9049be262dbaec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a00134f7baf6a4783dc1135145a361b",
+    "url": "/08-todo-react-bradh43/static/media/Roboto-Black.1a00134f.ttf"
+  },
+  {
+    "revision": "7f5ea97e2845502fd7628fb46e53cc31",
+    "url": "/08-todo-react-bradh43/static/media/KGPrimaryItalics.7f5ea97e.ttf"
+  },
+  {
+    "revision": "8aba274638d6911f5e9c",
+    "url": "/08-todo-react-bradh43/static/js/runtime~main.8aba2746.js"
+  },
+  {
+    "revision": "d7d7ee0add0593d6c50c",
+    "url": "/08-todo-react-bradh43/static/js/main.d7d7ee0a.chunk.js"
+  },
+  {
+    "revision": "3da870a3c1c4b41662f2",
+    "url": "/08-todo-react-bradh43/static/js/1.3da870a3.chunk.js"
+  },
+  {
+    "revision": "d7d7ee0add0593d6c50c",
+    "url": "/08-todo-react-bradh43/static/css/main.d5470c8b.chunk.css"
+  },
+  {
+    "revision": "4d539b3df14aa7db344cbb4b375c202f",
+    "url": "/08-todo-react-bradh43/index.html"
+  }
+];
